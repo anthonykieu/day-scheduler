db549a53d9d4a8fe7913d0eeaa4d40b6b8bcb7f5
Anthony H Kieu <anthonykieu1@Anthonys-MBP.fios-router.home>
2020-10-18 19:47:27 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,75 @@
+$("#currentDay").append(moment().format("MM-DD-YYYY"));
+// $("text8").html(localStorage.content);
+
+$(function()
+{
+    $("#button9").click(function()
+    {
+    var text=$("#text9").val();
+    localStorage.setItem("textarea9", text);
+    });
+});
+$(function()
+{
+    $("#button10").click(function()
+    {
+    var text=$("#text10").val();
+    localStorage.setItem("textarea10", text);
+    });
+});
+$(function()
+{
+    $("#button11").click(function()
+    {
+    var text=$("#text11").val();
+    localStorage.setItem("textarea11", text);
+    });
+});
+$(function()
+{
+    $("#button12").click(function()
+    {
+    var text=$("#text12").val();
+    localStorage.setItem("textarea12", text);
+    });
+});
+$(function()
+{
+    $("#button13").click(function()
+    {
+    var text=$("#text13").val();
+    localStorage.setItem("textarea13", text);
+    });
+});
+$(function()
+{
+    $("#button14").click(function()
+    {
+    var text=$("#text14").val();
+    localStorage.setItem("textarea14", text);
+    });
+});
+$(function()
+{
+    $("#button15").click(function()
+    {
+    var text=$("#text15").val();
+    localStorage.setItem("textarea15", text);
+    });
+});
+$(function()
+{
+    $("#button16").click(function()
+    {
+    var text=$("#text16").val();
+    localStorage.setItem("textarea16", text);
+    });
+});
+$(function()
+{
+    $("#button17").click(function()
+    {
+    var text=$("#text17").val();
+    localStorage.setItem("textarea17", text);
+    });
+});
